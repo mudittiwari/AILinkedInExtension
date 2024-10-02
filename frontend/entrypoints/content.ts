1abcea90fff5773146a843eeb5442bb34c6aad3c
mudit tiwari <mudit19.072@gmail.com>
2024-10-02 19:43:48 +0530

    added speech to text feature

--- frontend/entrypoints/content.ts
+++ frontend/entrypoints/content.ts
@@ -2,12 +2,12 @@ import icon from "~/assets/icon.png";
 import generate from "~/assets/generate.png";
 import insert from "~/assets/insert.png";
 import regenerate from "~/assets/regenerate.png";
-import micIcon from "~/assets/mic.png";
+import micIconImage from "~/assets/mic.png";
 import "~/assets/tailwind.css";
 export default defineContentScript({
   matches: ["*://*.linkedin.com/*"],
   main() {
-    //using observer so that code is executed when full page is loaded
+    // Using observer so that code is executed when the full page is loaded
     const observer = new MutationObserver(() => {
       const messageBoxes = document.getElementsByClassName(
         "msg-form__msg-content-container--scrollable scrollable relative"
@@ -15,18 +15,28 @@ export default defineContentScript({
       if (messageBoxes.length > 0) {
         let messageBox = messageBoxes[0];
         const handleClickOutside = (event: MouseEvent): void => {
-          //adding or removing the AI Icon when messaage box is focused
+          // Adding or removing the AI and mic icons when the message box is focused
           if (!messageBox.contains(event.target as Node)) {
-            let icon = document.getElementById("ai-icon");
-            if (icon) {
-              messageBox.removeChild(icon);
+            let aiIcon = document.getElementById("ai-icon");
+            let micIcon = document.getElementById("mic-icon-wrapper");
+            if (aiIcon) {
+              messageBox.removeChild(aiIcon);
+            }
+            if (micIcon) {
+              messageBox.removeChild(micIcon);
             }
           } else {
             let AIIcon = document.getElementById("ai-icon");
+            let micIcon = document.getElementById("mic-icon-wrapper");
             if (!AIIcon) {
-              const icon = createAIIcon();
-              icon.addEventListener("click", openModal);
-              messageBox.appendChild(icon);
+              const aiIconElement = createAIIcon();
+              aiIconElement.addEventListener("click", openModal);
+              messageBox.appendChild(aiIconElement);
+            }
+            if (!micIcon) {
+              const micIconElement = createMicIcon();
+              messageBox.appendChild(micIconElement);
+              addMicButtonEventListner();
             }
           }
         };
@@ -37,7 +47,7 @@ export default defineContentScript({
   },
 });
 
-//creating AI Icon using the image from figma
+// Creating AI Icon using the image from Figma
 const createAIIcon = (): HTMLImageElement => {
   const aiIcon = document.createElement("img") as HTMLImageElement;
   aiIcon.id = "ai-icon";
@@ -48,6 +58,25 @@ const createAIIcon = (): HTMLImageElement => {
     cursor-pointer
   `;
   return aiIcon;
+};
+
+// Creating Mic Icon
+const createMicIcon = (): HTMLDivElement => {
+  const micIconWrapper = document.createElement("div") as HTMLDivElement;
+  micIconWrapper.id = "mic-icon-wrapper";
+  micIconWrapper.className = `
+    absolute h-[27px] w-[27px]
+    right-16 bottom-[7px]
+    rounded-full bg-white flex items-center justify-center
+    shadow-md cursor-pointer
+  `;
+  const micIcon = document.createElement("img") as HTMLImageElement;
+  micIcon.id = "mic-icon";
+  micIcon.src = micIconImage;
+  micIcon.className = `h-6 w-6`;
+  micIconWrapper.appendChild(micIcon);
+
+  return micIconWrapper;
 };
 
 const openModal = (): void => {
@@ -70,7 +99,7 @@ const addPromptFieldEventListner = (): void => {
   showHideGenerateButton();
 };
 
-const showHideGenerateButton = (): void=>{
+const showHideGenerateButton = (): void => {
   const generateBtn = document.getElementById(
     "generate-btn"
   ) as HTMLButtonElement;
@@ -86,75 +115,81 @@ const showHideGenerateButton = (): void=>{
       }
     });
   }
-}
+};
 
 const addGenerateButtonEventListner = (): void => {
-  document.getElementById("generate-btn")?.addEventListener("click", async () => {
-    const userPrompt = (
-      document.getElementById("ai-command") as HTMLInputElement
-    ).value;
+  document
+    .getElementById("generate-btn")
+    ?.addEventListener("click", async () => {
+      const userPrompt = (
+        document.getElementById("ai-command") as HTMLInputElement
+      ).value;
 
-    if (userPrompt) {
-      const chatContainer = document.getElementById("chat-container");
+      if (userPrompt) {
+        const chatContainer = document.getElementById("chat-container");
 
-      // Create and append user message
-      const userMessage = document.createElement("div");
-      userMessage.className = `
+        // Create and append user message
+        const userMessage = document.createElement("div");
+        userMessage.className = `
         self-end p-3 rounded-lg max-w-[70%] break-words 
         bg-[#DFE1E7] text-[#666D80]
       `;
-      userMessage.textContent = userPrompt;
-      chatContainer?.appendChild(userMessage);
+        userMessage.textContent = userPrompt;
+        chatContainer?.appendChild(userMessage);
 
-      // Create and append server message with loading animation
-      const serverMessage = document.createElement("div");
-      serverMessage.className = `
+        // Create and append server message with loading animation
+        const serverMessage = document.createElement("div");
+        serverMessage.className = `
         self-start p-3 rounded-lg max-w-[70%] break-words 
         bg-[#DBEAFE] text-[#666D80] flex items-center
       `;
-      const loadingDots = document.createElement("span");
-      loadingDots.className = "loading-dots";
-      loadingDots.innerHTML = `
+        const loadingDots = document.createElement("span");
+        loadingDots.className = "loading-dots";
+        loadingDots.innerHTML = `
         <span class="dot">.</span>
         <span class="dot">.</span>
         <span class="dot">.</span>
       `;
-      serverMessage.appendChild(loadingDots);
-      chatContainer?.appendChild(serverMessage);
-      chatContainer?.scrollTo(0, chatContainer.scrollHeight);
-      const generateBtn = document.getElementById("generate-btn") as HTMLButtonElement;
-      const insertBtn = document.getElementById("insert-btn") as HTMLButtonElement;
-      const regenerateBtn = document.getElementById("regenerate-btn") as HTMLButtonElement;
+        serverMessage.appendChild(loadingDots);
+        chatContainer?.appendChild(serverMessage);
+        chatContainer?.scrollTo(0, chatContainer.scrollHeight);
+        const generateBtn = document.getElementById(
+          "generate-btn"
+        ) as HTMLButtonElement;
+        const insertBtn = document.getElementById(
+          "insert-btn"
+        ) as HTMLButtonElement;
+        const regenerateBtn = document.getElementById(
+          "regenerate-btn"
+        ) as HTMLButtonElement;
 
-      insertBtn.classList.remove("hidden");
-      regenerateBtn.classList.remove("hidden");
-      try {
-        const response = await fetch('http://localhost:5000/prompt', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt: userPrompt }),
-        });
-        const data = await response.json();
-        const serverResponse = data.message;
-        serverMessage.textContent = serverResponse;
-      } catch (error) {
-        serverMessage.textContent = "Error while connecting to the server.";
+        insertBtn.classList.remove("hidden");
+        regenerateBtn.classList.remove("hidden");
+        try {
+          const response = await fetch("https://ailinkedinextension.onrender.com/prompt", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: userPrompt }),
+          });
+          const data = await response.json();
+          const serverResponse = data.message;
+          serverMessage.textContent = serverResponse;
+        } catch (error) {
+          serverMessage.textContent = "Error while connecting to the server.";
+        }
+        if (serverMessage.textContent) {
+          saveMessagesToSession(userPrompt, serverMessage.textContent);
+        }
+        (document.getElementById("ai-command") as HTMLInputElement).value = "";
+        chatContainer?.scrollTo(0, chatContainer.scrollHeight);
       }
-      if(serverMessage.textContent){
-        saveMessagesToSession(userPrompt,serverMessage.textContent);
-      }
-      (document.getElementById("ai-command") as HTMLInputElement).value = "";
-      chatContainer?.scrollTo(0, chatContainer.scrollHeight);
-    }
-  });
+    });
 };
-
 
 const addInsertButtonEventListner = (): void => {
   document.getElementById("insert-btn")?.addEventListener("click", () => {
-
     const chatContainer = document.getElementById("chat-container");
     const serverMessages = chatContainer?.querySelectorAll(".self-start");
 
@@ -163,7 +198,6 @@ const addInsertButtonEventListner = (): void => {
         serverMessages[serverMessages.length - 1].textContent;
 
       if (lastServerMessage) {
-
         const messageBox = document.getElementsByClassName(
           "msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 full-height notranslate"
         )[0] as HTMLElement;
@@ -194,70 +228,74 @@ const addInsertButtonEventListner = (): void => {
   });
 };
 
-
 const addRegenerateButtonEventListner = (): void => {
-  document.getElementById("regenerate-btn")?.addEventListener("click", async () => {
-    const userPrompt = (
-      document.getElementById("ai-command") as HTMLInputElement
-    ).value;
+  document
+    .getElementById("regenerate-btn")
+    ?.addEventListener("click", async () => {
+      const userPrompt = (
+        document.getElementById("ai-command") as HTMLInputElement
+      ).value;
 
-    if (userPrompt) {
-      const chatContainer = document.getElementById("chat-container");
+      if (userPrompt) {
+        const chatContainer = document.getElementById("chat-container");
 
-      const serverMessages = chatContainer?.querySelectorAll(".self-start");
-      let lastServerMessage = "";
-      if (serverMessages && serverMessages.length > 0) {
-        lastServerMessage = serverMessages[serverMessages.length - 1].textContent || "";
-      }
-      const userMessage = document.createElement("div");
-      userMessage.className = `
+        const serverMessages = chatContainer?.querySelectorAll(".self-start");
+        let lastServerMessage = "";
+        if (serverMessages && serverMessages.length > 0) {
+          lastServerMessage =
+            serverMessages[serverMessages.length - 1].textContent || "";
+        }
+        const userMessage = document.createElement("div");
+        userMessage.className = `
         self-end p-3 rounded-lg max-w-[70%] break-words 
         bg-[#DFE1E7] text-[#666D80]
       `;
-      userMessage.textContent = userPrompt;
-      chatContainer?.appendChild(userMessage);
-      const serverMessage = document.createElement("div");
-      serverMessage.className = `
+        userMessage.textContent = userPrompt;
+        chatContainer?.appendChild(userMessage);
+        const serverMessage = document.createElement("div");
+        serverMessage.className = `
         self-start p-3 rounded-lg max-w-[70%] break-words 
         bg-[#DBEAFE] text-[#666D80] flex items-center
       `;
-      const loadingDots = document.createElement("span");
-      loadingDots.className = "loading-dots";
-      loadingDots.innerHTML = `
+        const loadingDots = document.createElement("span");
+        loadingDots.className = "loading-dots";
+        loadingDots.innerHTML = `
         <span class="dot">.</span>
         <span class="dot">.</span>
         <span class="dot">.</span>
       `;
-      serverMessage.appendChild(loadingDots);
-      chatContainer?.appendChild(serverMessage);
-      chatContainer?.scrollTo(0, chatContainer.scrollHeight);
-      try {
-        const response = await fetch('http://localhost:5000/prompt', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ prompt:lastServerMessage+"........"+userPrompt }),
-        });
-        if (response.ok) {
-          const data = await response.json();
-          const serverResponse = data.message || "Server response here";
-          serverMessage.textContent = serverResponse;
-        } else {
-          serverMessage.textContent = "Failed to get response from the server.";
+        serverMessage.appendChild(loadingDots);
+        chatContainer?.appendChild(serverMessage);
+        chatContainer?.scrollTo(0, chatContainer.scrollHeight);
+        try {
+          const response = await fetch("https://ailinkedinextension.onrender.com/prompt", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: lastServerMessage + "........" + userPrompt,
+            }),
+          });
+          if (response.ok) {
+            const data = await response.json();
+            const serverResponse = data.message || "Server response here";
+            serverMessage.textContent = serverResponse;
+          } else {
+            serverMessage.textContent =
+              "Failed to get response from the server.";
+          }
+        } catch (error) {
+          serverMessage.textContent = "Error while connecting to the server.";
         }
-      } catch (error) {
-        serverMessage.textContent = "Error while connecting to the server.";
+        if (serverMessage.textContent) {
+          saveMessagesToSession(userPrompt, serverMessage.textContent);
+        }
+        (document.getElementById("ai-command") as HTMLInputElement).value = "";
+        chatContainer?.scrollTo(0, chatContainer.scrollHeight);
       }
-      if(serverMessage.textContent){
-        saveMessagesToSession(userPrompt,serverMessage.textContent);
-      }
-      (document.getElementById("ai-command") as HTMLInputElement).value = "";
-      chatContainer?.scrollTo(0, chatContainer.scrollHeight);
-    }
-  });
+    });
 };
-
 
 const createModal = (): HTMLElement => {
   const modal = document.createElement("div");
@@ -274,7 +312,7 @@ const createModal = (): HTMLElement => {
     <div class="flex justify-between">
     <input id="ai-command" type="text" style="outline:none;border:0px solid black;" placeholder="Your Promt" class="p-2 rounded-md w-full mr-3" />
      <button title="Press and hold for recording audio." id="mic-btn" class="bg-gray-200 rounded-full p-2 w-[32px] flex justify-center items-center">
-        <img src="${micIcon}" alt="Mic" class="w-6 h-6" />
+        <img src="${micIconImage}" alt="Mic" class="w-6 h-6" />
       </button>
     </div>
     <div class="flex space-x-2 justify-end w-full">
@@ -298,10 +336,13 @@ const createModal = (): HTMLElement => {
 
   modal.appendChild(modalContent);
 
-
   const micBtn = modalContent.querySelector("#mic-btn") as HTMLButtonElement;
-  const aiCommandInput = modalContent.querySelector("#ai-command") as HTMLInputElement;
-  const SpeechRecognition = (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+  const aiCommandInput = modalContent.querySelector(
+    "#ai-command"
+  ) as HTMLInputElement;
+  const SpeechRecognition =
+    (window as any).SpeechRecognition ||
+    (window as any).webkitSpeechRecognition;
   if (SpeechRecognition) {
     const recognition = new SpeechRecognition();
     recognition.continuous = false;
@@ -316,7 +357,7 @@ const createModal = (): HTMLElement => {
     recognition.onresult = (event: any) => {
       const transcript = event.results[0][0].transcript;
       aiCommandInput.value += transcript;
-      const inputEvent = new Event('input', { bubbles: true });
+      const inputEvent = new Event("input", { bubbles: true });
       aiCommandInput.dispatchEvent(inputEvent);
     };
     recognition.onerror = (event: any) => {
@@ -328,34 +369,88 @@ const createModal = (): HTMLElement => {
   return modal;
 };
 
+const addMicButtonEventListner = (): void => {
+  let micIcon = document.getElementById("mic-icon-wrapper");
+  const SpeechRecognition =
+    (window as any).SpeechRecognition ||
+    (window as any).webkitSpeechRecognition;
+  if (micIcon) {
+    if (SpeechRecognition) {
+      const recognition = new SpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = false;
+      micIcon.addEventListener("mousedown", () => {
+        recognition.start();
+      });
 
-const saveMessagesToSession = (userMessage: string, serverMessage: string): void => {
-  let chatHistory = JSON.parse(sessionStorage.getItem('chatHistory') || '[]');
+      micIcon.addEventListener("mouseup", () => {
+        recognition.stop();
+      });
+      recognition.onresult = (event: any) => {
+        const transcript = event.results[0][0].transcript;
+        const messageBox = document.getElementsByClassName(
+          "msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 full-height notranslate"
+        )[0] as HTMLElement;
+
+        if (messageBox) {
+          const pTag = messageBox.querySelector("p");
+
+          if (pTag) {
+            pTag.textContent = transcript;
+            const placeholderDiv = document.querySelector(
+              ".msg-form__placeholder"
+            );
+            if (placeholderDiv) {
+              placeholderDiv.classList.remove("msg-form__placeholder");
+            }
+          }
+        }
+      };
+      recognition.onerror = (event: any) => {
+        console.error("Speech recognition error", event.error);
+      };
+    } else {
+      console.error("Speech recognition not supported in this browser.");
+    }
+  }
+};
+
+const saveMessagesToSession = (
+  userMessage: string,
+  serverMessage: string
+): void => {
+  let chatHistory = JSON.parse(sessionStorage.getItem("chatHistory") || "[]");
   chatHistory.push({ userMessage, serverMessage });
-  sessionStorage.setItem('chatHistory', JSON.stringify(chatHistory));
-  console.log(chatHistory);
+  sessionStorage.setItem("chatHistory", JSON.stringify(chatHistory));
 };
 
 const restoreMessagesFromSession = (): void => {
-  const chatHistory = JSON.parse(sessionStorage.getItem('chatHistory') || '[]');
-  console.log(chatHistory);
+  const chatHistory = JSON.parse(sessionStorage.getItem("chatHistory") || "[]");
   const chatContainer = document.getElementById("chat-container");
-  chatHistory.forEach(({ userMessage, serverMessage }: { userMessage: string, serverMessage: string }) => {
-    const userMessageDiv = document.createElement("div");
-    userMessageDiv.className = `
+  chatHistory.forEach(
+    ({
+      userMessage,
+      serverMessage,
+    }: {
+      userMessage: string;
+      serverMessage: string;
+    }) => {
+      const userMessageDiv = document.createElement("div");
+      userMessageDiv.className = `
       self-end p-3 rounded-lg max-w-[70%] break-words 
       bg-[#DFE1E7] text-[#666D80]
     `;
-    userMessageDiv.textContent = userMessage;
-    chatContainer?.appendChild(userMessageDiv);
-    const serverMessageDiv = document.createElement("div");
-    serverMessageDiv.className = `
+      userMessageDiv.textContent = userMessage;
+      chatContainer?.appendChild(userMessageDiv);
+      const serverMessageDiv = document.createElement("div");
+      serverMessageDiv.className = `
       self-start p-3 rounded-lg max-w-[70%] break-words 
       bg-[#DBEAFE] text-[#666D80]
     `;
-    serverMessageDiv.textContent = serverMessage;
-    chatContainer?.appendChild(serverMessageDiv);
-  });
+      serverMessageDiv.textContent = serverMessage;
+      chatContainer?.appendChild(serverMessageDiv);
+    }
+  );
 
   chatContainer?.scrollTo(0, chatContainer.scrollHeight);
 };
